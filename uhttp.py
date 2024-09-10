495dd4d089574a1e88619e3cfb07f7d7db86510c
Agustin Arce <agustinarceprincich@gmail.com>
2024-09-10 18:12:37 -0400

    docs (uhttp): update MultiDict dosctring

--- uhttp.py
+++ uhttp.py
@@ -578,7 +578,49 @@ async def asyncfy(func, /, *args, **kwargs):
 
 
 class MultiDict(dict):
-    """Used for internal usage"""
+    """
+    Support for multipart forms. Depends on [python-multipart](https://pypi.org/project/python-multipart/).
+    
+    E.g.
+
+    ```python
+    from multipart.multipart import FormParser, parse_options_header
+    from multipart.exceptions import FormParserError
+    from uhttp import Application, MultiDict, Response
+
+    app = Application()
+
+    def parse_form(request):
+        form = MultiDict()
+
+        def on_field(field):
+            form[field.field_name.decode()] = field.value.decode()
+        def on_file(file):
+            if file.field_name:
+                form[file.field_name.decode()] = file.file_object
+        content_type, options = parse_options_header(
+            request.headers.get('content-type', '')
+        )
+        try:
+            parser = FormParser(
+                content_type.decode(),
+                on_field,
+                on_file,
+                boundary=options.get(b'boundary'),
+                config={'MAX_MEMORY_FILE_SIZE': float('inf')}  # app._max_content
+            )
+            parser.write(request.body)
+            parser.finalize()
+        except FormParserError:
+            raise Response(400)
+        return form
+
+    @app.before
+    def handle_multipart(request):
+        if 'multipart/form-data' in request.headers.get('content-type'):
+            request.form = parse_form(request)
+    ```
+    """
 
     def __init__(
         self,
